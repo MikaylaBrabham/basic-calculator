afb8ce20659c353d7b33263799eea55f07782e47
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-19 05:50:16 -0500

    First real crashout moment occured due to forgetting to insert a main - inserted variables to gather from others - note for next time: set a timer for 10 minutes and once that goes off its time to get external help

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+// Import Scanner for  user to input
+import java.util.*;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+
+        // Add my scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Tell user to input their first variable
+        System.out.print("What is your first number? ");
+        float firstVariable = scanner.nextFloat();
+
+        // Tell user to input their first variable
+        System.out.print("What is your second number? ");
+        float secondVariable = scanner.nextFloat();
+
+
+
+        // Display variable
+        //System.out.println("Enter the second number: " + secondVariable);
+
+
+        //Tell user to input their first variable in 2 point float
+        //Display Variable
+
+        //Give possible calculations options
+
+        //Display the calculation options user can pick from
+    }
+}
